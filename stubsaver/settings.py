d523b630ca5f285ad15d3f85423cf1b9ccee9752
tpett20 <thomaspettersen20@gmail.com>
2023-04-18 11:14:07 -0400

    create main_app and connect database

--- stubsaver/settings.py
+++ stubsaver/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'stubsaver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stubsaver',
     }
 }
 
