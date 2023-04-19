cad500fbbf6f1494011d3d6382a8711e5bc3a5e5
kearmododragon <ckearney1992@gmail.com>
2023-04-19 20:08:18 +0200

    artist model and functionality started

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ticket, Highlight
+from .models import Artist, Ticket, Highlight
 
 # Register your models here.
+admin.site.register(Artist)
 admin.site.register(Ticket)
 admin.site.register(Highlight)
--- main_app/models.py
+++ main_app/models.py
@@ -9,12 +9,20 @@ EVENT_TYPES = (
 )
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Ticket(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     companion = models.CharField(max_length=250)
     event_type = models.CharField(max_length=1, choices=EVENT_TYPES, default=EVENT_TYPES[0][0])
+    artists = models.ManyToManyField(Artist)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
